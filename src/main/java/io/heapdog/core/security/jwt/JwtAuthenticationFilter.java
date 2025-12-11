ff9997ec16539b6ebc7b9446410455e3747de85e
parthokr <partho.kr@proton.me>
2025-12-12 03:01:43 +0600

    fix(filter): use auth entrypoint in jwt

--- src/main/java/io/heapdog/core/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/io/heapdog/core/security/jwt/JwtAuthenticationFilter.java
@@ -9,18 +9,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @AllArgsConstructor
-@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtAuthenticationService jwtAuthenticationService;
-    private final HandlerExceptionResolver handlerExceptionResolver;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
@@ -29,17 +26,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String authHeader = request.getHeader("authorization");
 
-        if (authHeader != null && authHeader.startsWith("Bearer")) {
-            String token = authHeader.substring(7);
-            try {
-                Authentication authentication = jwtAuthenticationService.verifyToken(token);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            } catch (JwtValidationFailedException e) {
-                handlerExceptionResolver.resolveException(request, response, null, e);
-                return;
-            }
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        try {
+            Authentication authentication = jwtAuthenticationService.verifyToken(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (JwtValidationFailedException e) {
+            jwtAuthenticationEntryPoint.commence(request, response, e);
+            return;
         }
         filterChain.doFilter(request, response);
-
     }
 }
