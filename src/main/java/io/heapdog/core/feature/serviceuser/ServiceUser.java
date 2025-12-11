688f0bd7202498f3df68f2e33fa8520b25e68378
parthokr <partho.kr@proton.me>
2025-12-12 02:56:36 +0600

    feat(entity): add ServiceUser entity

---
+++ src/main/java/io/heapdog/core/feature/serviceuser/ServiceUser.java
@@ -0,0 +1,34 @@
+package io.heapdog.core.feature.serviceuser;
+
+
+import io.heapdog.core.shared.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Builder
+@Entity
+@Table(name = "heapdog_service_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceUser extends BaseEntity {
+
+    private String name;
+    private String apiKey;
+    private boolean enabled;
+
+    @ElementCollection(targetClass = ServiceUserPermission.class, fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "heapdog_service_user_permission",
+            joinColumns = @JoinColumn(name = "service_user_id")
+    )
+    @Column(name = "permission")
+    @Enumerated(EnumType.STRING)
+    private Set<ServiceUserPermission> permissions;
+
+
+
+}
