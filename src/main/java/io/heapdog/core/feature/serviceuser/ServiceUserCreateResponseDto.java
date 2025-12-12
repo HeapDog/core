d5206f810065cbd689a695227a695e6aed8f6642
parthokr <partho.kr@proton.me>
2025-12-13 00:35:18 +0600

    fix(dto): handle permission while creating/reading a service user

--- src/main/java/io/heapdog/core/feature/serviceuser/ServiceUserCreateResponseDto.java
+++ src/main/java/io/heapdog/core/feature/serviceuser/ServiceUserCreateResponseDto.java
@@ -4,12 +4,14 @@ package io.heapdog.core.feature.serviceuser;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 @Builder
 public class ServiceUserCreateResponseDto {
-
     private Long id;
     private String name;
     private String apiKey;
     private Boolean enabled;
+    private Set<ServiceUserPermission> permissions;
 }
