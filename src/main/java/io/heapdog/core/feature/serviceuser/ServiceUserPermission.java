86b97ae30b23cae00640fd7369287bca72449f5d
parthokr <partho.kr@proton.me>
2025-12-13 01:25:08 +0600

    feat(permission): add new permission READ_SSE_TOKEN

--- src/main/java/io/heapdog/core/feature/serviceuser/ServiceUserPermission.java
+++ src/main/java/io/heapdog/core/feature/serviceuser/ServiceUserPermission.java
@@ -5,6 +5,8 @@ public enum ServiceUserPermission {
     READ_HEAPDOG_USER("read:heapdog_user"),
     WRITE_HEAPDOG_USER("write:heapdog_user"),
     READ_NOTIFICATION("read:notification");
+    READ_NOTIFICATION("read:notification"),
+    READ_SSE_TOKEN("read:sse_token");
 
     private final String permission;
 
