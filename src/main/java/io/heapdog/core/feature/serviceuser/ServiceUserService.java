e191154a739daa244b2a7ebe399d0df37978b780
parthokr <partho.kr@proton.me>
2025-12-13 00:22:42 +0600

    feat(permission): handle permissions for service user

--- src/main/java/io/heapdog/core/feature/serviceuser/ServiceUserService.java
+++ src/main/java/io/heapdog/core/feature/serviceuser/ServiceUserService.java
@@ -1,9 +1,12 @@
 package io.heapdog.core.feature.serviceuser;
 
+import io.heapdog.core.shared.ResourceNotFoundException;
 import io.heapdog.core.shared.util.OtpGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class ServiceUserService {
                 .name(request.getName())
                 .apiKey(String.format("svc-%s", OtpGenerator.generateOtp(32)))
                 .enabled(true)
+                .permissions(request.getPermissions())
                 .build();
         var saved = serviceUserRepository.save(serviceUser);
         return ServiceUserCreateResponseDto.builder()
@@ -24,6 +28,43 @@ public class ServiceUserService {
                 .name(saved.getName())
                 .apiKey(saved.getApiKey())
                 .enabled(saved.isEnabled())
+                .permissions(saved.getPermissions())
+                .build();
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    ServiceUserReadResponseDto getServiceUser(Long id) {
+        var serviceUser = serviceUserRepository.findByIdWithPermissionsAndCreator(id)
+                .orElseThrow(() -> new ResourceNotFoundException("ServiceUser", "id", id.toString()));
+        return ServiceUserReadResponseDto.builder()
+                .id(serviceUser.getId())
+                .name(serviceUser.getName())
+                .apiKey(serviceUser.getApiKey())
+                .enabled(serviceUser.isEnabled())
+                .createdAt(serviceUser.getCreatedAt())
+                .updatedAt(serviceUser.getUpdatedAt())
+                .createdBy(ServiceUserReadResponseDto.User.builder()
+                        .id(serviceUser.getCreatedBy().getId())
+                        .username(serviceUser.getCreatedBy().getUsername())
+                        .build())
+                .permissions(serviceUser.getPermissions())
+                .lastAccessedAt(serviceUser.getLastAccessedAt())
+                .build();
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    ServiceUserPermissionResponseDto getServiceUserPermissions() {
+        var permissions = Arrays.stream(ServiceUserPermission.values())
+                .map(permission -> ServiceUserPermissionResponseDto.Permission.builder()
+                        .name(permission.name())
+                        .label(permission.getLabel())
+                        .description(permission.getDescription())
+                        .build())
+                .toList();
+        return ServiceUserPermissionResponseDto.builder()
+                .permissions(permissions)
                 .build();
     }
 }
