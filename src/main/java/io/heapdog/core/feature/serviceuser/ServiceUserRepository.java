c5b9e091b4d901cf9f42df0cf72e008f4994b884
parthokr <partho.kr@proton.me>
2025-12-13 00:23:23 +0600

    feat(api): add new api findByIdWithPermissionsAndCreator

--- src/main/java/io/heapdog/core/feature/serviceuser/ServiceUserRepository.java
+++ src/main/java/io/heapdog/core/feature/serviceuser/ServiceUserRepository.java
@@ -1,11 +1,21 @@
 package io.heapdog.core.feature.serviceuser;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ServiceUserRepository extends JpaRepository<ServiceUser, Long> {
 
     Optional<ServiceUser> findByApiKey(String apiKey);
+
+    @Query("""
+              SELECT DISTINCT s
+              FROM ServiceUser s
+              LEFT JOIN FETCH s.permissions
+              LEFT JOIN FETCH s.createdBy
+              WHERE s.id = :id
+            """)
+    Optional<ServiceUser> findByIdWithPermissionsAndCreator(Long id);
 
 }
